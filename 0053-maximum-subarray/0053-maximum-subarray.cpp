8eb3d1893725797b30eaadcedbe4737317007cbc
Prajwal Bhosale <bprajwal1803@gmail.com>
2026-08-24 12:14:30 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.48%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int sum=0;
+       int n = nums.size();
+       long long maxi = LONG_MIN;
+       for(int i=0;i<n;i++) 
+        {
+            sum += nums[i];
+            if(sum>maxi) maxi=sum;
+            if(sum<0) sum=0;
+        } 
+        return maxi;
+        }
+};
