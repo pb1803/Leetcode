390f47c4ed0f4fef91dc26eecedb4bb677ed5f8c
Prajwal Bhosale <bprajwal1803@gmail.com>
2026-08-26 13:05:05 +0530

    Time: 408 ms (90.27%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,5 @@
+SELECT today.id
+FROM Weather today
+JOIN Weather yesterday
+    ON today.recordDate = DATE_ADD(yesterday.recordDate, INTERVAL 1 DAY)
+WHERE today.temperature > yesterday.temperature;
