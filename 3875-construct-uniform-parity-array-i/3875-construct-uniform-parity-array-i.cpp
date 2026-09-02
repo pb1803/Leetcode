9303780b864f117c3db4cd4085f99ea07e336e8b
Prajwal Bhosale <bprajwal1803@gmail.com>
2026-09-02 23:49:38 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+        // int cnt_o =0;
+        // int cnt_e =0;
+        // for(int i=0;i<nums1.size();i++)
+        // {
+        //    if(nums1[i]%2==0) cnt_e++;
+        //    else cnt_o++;
+        // }
+        // if(cnt_o==0 || cnt_e==0) return true;
+
+        // if(cnt_o == cnt_e || cnt_o==2*cnt_e || cnt_e==2*cnt_o) return true;
+        // else return false;
+    }
+};
