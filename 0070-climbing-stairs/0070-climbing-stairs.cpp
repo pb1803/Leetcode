a20bd07a70449ed1c2daab081c22f779affb3696
Prajwal Bhosale <bprajwal1803@gmail.com>
2026-09-09 12:32:51 +0530

    Time: 0 ms (100%), Space: 8 MB (59.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int climbStairs(int n) {
         int prev,prev2,curr;
-        if(n==0) prev2=1;
-        if(n==1) prev=1;
+        prev2=1;
+        prev=1;
         for(int i=2;i<=n;i++)
         {
             curr=prev+prev2;
